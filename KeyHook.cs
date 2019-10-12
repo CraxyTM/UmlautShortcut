9b505a80e9023d870ef1743bfd42f74b7e39a910
Craxy <16610792+CraxyTM@users.noreply.github.com>
2019-10-12 21:41:57 +0200

    Added low level keyboard hook

---
+++ KeyHook.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace UmlautShortcut
+{
+    public class KeyHook
+    {
+        private const int WhKeyboardLl = 13;
+        private const int WmKeydown = 0x0100;
+        private const int WmSysKeyDown = 0x0104;
+
+        private static IntPtr _hookId = IntPtr.Zero;
+
+        public static void Start()
+        {
+            _hookId = SetHook(HookCallback);
+        }
+
+        public static void Stop()
+        {
+            UnhookWindowsHookEx(_hookId);
+        }
+
+        private static IntPtr SetHook(KeyboardProc proc)
+        {
+            using (var process = Process.GetCurrentProcess())
+            using (var module = process.MainModule)
+            {
+                return SetWindowsHookEx(WhKeyboardLl, proc, GetModuleHandle(module?.ModuleName), 0);
+            }
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr) WmKeydown || wParam == (IntPtr) WmSysKeyDown))
+            {
+                Console.WriteLine("Key Pressed");
+            }
+
+            return CallNextHookEx(_hookId, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private delegate IntPtr KeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+    }
+}
